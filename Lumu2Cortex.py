d707460d4a695f31c2773f84ebf9cff4396253ea
alan7s <alan7@outlook.com>
2024-03-13 15:04:20 -0300

    Update Lumu2Cortex.py v0.3

--- Lumu2Cortex.py
+++ Lumu2Cortex.py
@@ -1,11 +1,9 @@
 import requests
 import os
 from dotenv import load_dotenv
-import pandas as pd
-import time
 
 # Upload LUMU IOCs to Cortex XDR Blocklist
-# Version v0.2 by alan7s
+# Version v0.3 by alan7s
 
 def cortexCheck(api, id, fqdn, blocklist, comment):
     headers = {
@@ -26,29 +24,36 @@ def chunks(lista, n):
     for i in range(0, len(lista), n):
         yield lista[i:i + n]
 
+def getLUMU(id,api):
+    url = f'https://defender.lumu.io/api/incidents/{id}/context?key={api}&hash=sha256'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['related_files']
+    else:
+        print("Falha na requisição:", response.status_code)
+        return []
+
 def main():
     # Carregando as variáveis de ambiente do arquivo .env
     load_dotenv(override=True)
-    '''.env file content example:
-        cortex_api = "API_KEY"
-        cortex_id = "ID"
-        cortex_fqdn = "fqdn"
-    '''
-    cortex_api = os.getenv("cortex_responder_api")
-    cortex_id = os.getenv("cortex_responder_id")
-    cortex_fqdn = os.getenv("cortex_fqdn")
-        
-    df = pd.read_csv('lumuiocs.csv') #set your lumu.csv path
-    #print(df)
-    blocklist = df[' sha256'].tolist()
-    print("IOCs loaded")
-    listofblocklist = list(chunks(blocklist, 100))
-    print(f'IOCs: {len(blocklist)}')
-    print(f'Sublistas: {len(listofblocklist)}')
-    comment = input("Add comment: ")
-    for i in range(len(listofblocklist)):      
-        cortexCheck(cortex_api, cortex_id, cortex_fqdn, listofblocklist[i], comment)
-        time.sleep(5)
+
+    tenant = input("Tenant: ")
+
+    cortex_api = os.getenv(f"cortex_responder_api_{tenant}")
+    cortex_id = os.getenv(f"cortex_responder_id_{tenant}")
+    cortex_fqdn = os.getenv(f"cortex_fqdn_{tenant}")
+    lumu_api = os.getenv(f"lumu_defender_api_{tenant}")
+
+    idLUMU = input("Incident ID: ")
+    related_files = getLUMU(idLUMU,lumu_api)
+    listofblocklist = list(chunks(related_files, 100))
+    if len(listofblocklist) > 0:
+        print(f'IOCs: {len(related_files)}')
+        print(f'Sublistas: {len(listofblocklist)}')
+        comment = input("Add comment: ")
+        for i in range(len(listofblocklist)):      
+            cortexCheck(cortex_api, cortex_id, cortex_fqdn, listofblocklist[i], comment)
 
 if __name__ == "__main__":
     main()
