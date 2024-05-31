a5ea65ceabc5055544b2f809fc9654c2b673ad76
alan7s <alan7@outlook.com>
2024-05-31 11:32:34 -0300

    Update url2domain.py

--- url2domain.py
+++ url2domain.py
@@ -4,24 +4,25 @@ def url2domain(arquivo_entrada, arquivo_saida):
     try:
         with open(arquivo_entrada, 'r') as entrada:
             urls = entrada.readlines()
-        domains = []
+        domains = set()
         for url in urls:
             url = url.strip()
             parsed_url = tldextract.extract(url)
-            domains.append(parsed_url.registered_domain)
-            print(parsed_url.registered_domain)
+            domains.add(parsed_url.registered_domain)
 
         with open(arquivo_saida, 'w') as saida:
             for domain in domains:
-                saida.write(domain + '\n')
+                if domain:
+                    print(domain)
+                    saida.write(domain + '\n')
         print(f"Domínios extraídos com sucesso e salvos em {arquivo_saida}")
     except FileNotFoundError:
         print(f"Arquivo {arquivo_entrada} não encontrado.")
     
 
 def main():
-    input = 'urlList.txt' 
-    output = 'domainList.txt' 
+    input = 'listURL.txt' 
+    output = 'listDomain.txt' 
     url2domain(input, output)
 
 if __name__ == "__main__":
