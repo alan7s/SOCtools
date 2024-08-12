1b317cebbd31fcdf1820663c293c8386f3d9ff22
Matheus Motta <matheusreismottajf@gmail.com>
2024-08-12 13:30:48 -0300

    Bugs fixes and Add new features to socIPcheck.py (#2)
    * modified
    
    * Bugs fixes and Add new features to socIPcheck.py

--- socIPcheck.py
+++ socIPcheck.py
@@ -1,3 +1,5 @@
+import socket
+import sys
 import requests
 import shodan
 import argparse
@@ -15,17 +17,16 @@ def abuseIPDB(ip, api_key):
         "Key": api_key  # AbuseIPDB API Key
     }
     response = requests.get(url, headers=headers)
-    print("AbuseIPDB scan:")
+    print("\n[+] AbuseIPDB scan:\n")
     if response.status_code == 200:
         data = response.json()
         abuse_confidence_score = data['data']['abuseConfidenceScore']
         if abuse_confidence_score > 0:
-            print(f'. {ip} has an abuse confidence score of {abuse_confidence_score}/100. See https://www.abuseipdb.com/check/{ip}')
+            print(f'\t. {ip} has an abuse confidence score of {abuse_confidence_score}/100. See https://www.abuseipdb.com/check/{ip}')
         else:
-            print(f'. No abusive activity found for {ip}. See https://www.abuseipdb.com/check/{ip}')
+            print(f'\t. No abusive activity found for {ip}. See https://www.abuseipdb.com/check/{ip}')
     else:
-        print(f"Failed to fetch data. Status Code: {response.status_code}")
-    print()
+        print(f"\tFailed to fetch data. Status Code: {response.status_code}")
 
 def vtScan(ip,inpt, api):
     if inpt:
@@ -37,7 +38,7 @@ def vtScan(ip,inpt, api):
         "x-apikey": api  # Virustotal API KEY
     }
     response = requests.get(url, headers=headers)
-    print("VirusTotal scan:")
+    print(f"\n[+] VirusTotal {'IP' if inpt else "Domain"} scan:\n")
     if response.status_code == 200:
         data = response.json()
         last_analysis_stats = data['data']['attributes']['last_analysis_stats']
@@ -48,39 +49,41 @@ def vtScan(ip,inpt, api):
         # Get malicious stats x/
         malicious = last_analysis_stats['malicious']
         if inpt:
-            print(f'. {malicious}/{total} security vendors flagged {ip}. See https://www.virustotal.com/gui/ip-address/{ip}') # VirusTotal scan: x/y
+            print(f'\t. {malicious}/{total} security vendors flagged {ip}. See https://www.virustotal.com/gui/ip-address/{ip}') # VirusTotal scan: x/y
         else:
-            print(f'. {malicious}/{total} security vendors flagged {ip}. See https://www.virustotal.com/gui/domain/{ip}')
+            print(f'\t. {malicious}/{total} security vendors flagged {ip}. See https://www.virustotal.com/gui/domain/{ip}')
     else:
          print(f"Failed to fetch data. Status Code: {response.status_code}")
-    print()
 
-def shodanScan(target, api):
-    api_key = api #Shodan API KEY
+def shodanScan(target, api_key):
     api = shodan.Shodan(api_key)
-    print("Shodan scan: ")
     try:
         results = api.host(target)
-        print(f". Organization: {results.get('org', 'N/A')}")
+        print("\n[+] Shodan scan:\n")
+        print(f"\t. Organization: {results.get('org', 'N/A')}")
         domains = results.get('domains', [])
-        if domains:
-            print(f". Domains: {', '.join(map(str, domains))}")
-        else:
-            print(". Domains: N/A")
+        print(f"\t. Domains: {', '.join(map(str,domains)) if domains else 'Domains: N/A'}")
         ports = results.get('ports', [])
-        if ports:
-            print(f". Ports: {', '.join(map(str, ports))}")
-        else:
-            print(". Ports: N/A")
+        print(f"\t. Ports: {', '.join(map(str,ports)) if ports else 'Ports: N/A'}")
         vulnerabilities = results.get('vulns', [])
-        if vulnerabilities:
-            print(f". Vulnerabilities: {', '.join(map(str, vulnerabilities))}")
-        else:
-            print(". Vulnerabilities: N/A")
-        print(f'. Source https://www.shodan.io/host/{target}')
+        print(f"\t. Vulnerabilities: {', '.join(map(str,vulnerabilities)) if vulnerabilities else 'Vulnerabilities: N/A'}")
+        print(f'\t. Source https://www.shodan.io/host/{target}\n')
+        return 1
     except shodan.APIError as e:
-       print(f"Error: {e}")
-    print()
+       return 0
+    
+def shodanFreeScan(target):
+    print("\n[+] Shodan scan:\n")
+    try:
+        results = requests.get(f"https://internetdb.shodan.io/{target}").json()
+        print(f"\t. Hostnames: {', '.join(map(str, results['hostnames'])) if results['hostnames'] else 'Hostnames: N/A'}")
+        print(f"\t. Ports: {', '.join(map(str,results['ports'])) if results['ports'] else 'Ports: N/A'}")
+        print(f"\t. Vulnerabilities: {', '.join(map(str, results['vulns'])) if results['vulns'] else 'Vulnerabilities: N/A'}")
+        print(f'\t. Source https://www.shodan.io/host/{target}\n')
+        return 1
+    except Exception as e:
+       print(f"\tError: {e}\n")
+       return 0
 
 def cortexCheck(ip, api, id, fqdn):
     headers = {
@@ -99,8 +102,15 @@ def cortexCheck(ip, api, id, fqdn):
     res = requests.post(url=f'https://api-{fqdn}.xdr.us.paloaltonetworks.com/public_api/v1/endpoints/get_endpoint/', # Set domain name
 						headers=headers,
 						json=parameters)
+
+    print("\n[+] Cortex endpoint:\n")
+    if res.status_code == 401:
+        print(f"Cortex API: Unauthorized. Status Code: {res.status_code}")
+        return 0
+    elif res.status_code != 200:
+        print(f"Cortex API: Error. Status Code: {res.status_code}")
+        return 0
     cortex = res.json()
-    print("Cortex endpoint: ")
     try:
         endpoint = cortex['reply']['endpoints'][0]
         lastseen = endpoint['last_seen']
@@ -109,18 +119,18 @@ def cortexCheck(ip, api, id, fqdn):
         date = date - timedelta(hours=3)
         format_date = '%b %d, %Y, %I:%M %p'
         lastseen = date.strftime(format_date)
-        print(f". Name: {endpoint['endpoint_name']}")
-        print(f". Type: {endpoint['endpoint_type']}")
-        print(f". Status: {endpoint['endpoint_status']}")
-        print(f". User: {endpoint['users']}")
-        print(f". OS: {endpoint['os_type']}")
-        print(f". Agent version: {endpoint['endpoint_version']}")
-        print(f". IP address: {endpoint['ip']}")
-        print(f". Last seen: {lastseen}")
-        print()
-        print(f"Machine {endpoint['endpoint_name']} with Cortex {endpoint['endpoint_status']} last seen in {lastseen}")
+        print(f"\t. Name: {endpoint['endpoint_name']}")
+        print(f"\t. Type: {endpoint['endpoint_type']}")
+        print(f"\t. Status: {endpoint['endpoint_status']}")
+        print(f"\t. User: {', '.join(endpoint['users'])}")
+        print(f"\t. OS: {endpoint['os_type']}")
+        print(f"\t. Agent version: {endpoint['endpoint_version']}")
+        print(f"\t. IP address: {', '.join(endpoint['ip'])}")
+        print(f"\t. Last seen: {lastseen}\n")
+        print(f"Machine {endpoint['endpoint_name']} is with Cortex {endpoint['endpoint_status']} last seen in {lastseen}")
+        return 1
     except IndexError:
-        print(f"{ip} not found")
+        print(f"\t .{ip} not found\n")
 
 def cortexMalwareScan(api, id, fqdn, ip):
     headers = {
@@ -142,76 +152,103 @@ def cortexMalwareScan(api, id, fqdn, ip):
     response = requests.post(url, json=payload, headers=headers)
     print(response.json())
 
+def banner():
+    print("""
+             ,-----------------------------,      
+        ,------------------------------------,    
+      ,"                                   ,"|    
+     +------------------------------------+  |
+     |  .------------------------------.  |  |
+     |  |┌──(alan7s㉿PC-666)-[SOCtools]|  |  |
+     |  |└─$ ./socIPcheck.py           |  |  |
+     |  | ┌─┐┌─┐┌─┐╦╔═╗┌─┐┬ ┬┌─┐┌─┐┬┌─ |  |  |
+     |  | └─┐│ ││  ║╠═╝│  ├─┤├┤ │  ├┴┐ |  |  |
+     |  | └─┘└─┘└─┘╩╩  └─┘┴ ┴└─┘└─┘┴ ┴ |  |  |
+     |  |           ...                |  |  |/----
+     |  |┌──(alan7s㉿PC-666)-[SOCtools]|  |  |   ,/
+     |  |└─$                           |  |  |  //
+     |  `------------------------------'  |," .;
+     +------------------------------------+  ;;
+        /_)___________________________(_/  //'
+       ___________________________/___
+      /  oooooooooooooooo  .o.  oooo /,
+     / ==ooooooooooooooo==.o.  ooo= //
+    /_==__==========__==_ooo__ooo=_/'
+    `-----------------------------'""")
+
 def main():
-    try:
-        parser = argparse.ArgumentParser(description='Scan IP address using VirusTotal, Shodan and Cortex XDR.')
-        parser.add_argument('-r', '--remote', dest='remote_ip', required=False, help='Remote IP address to scan')
-        parser.add_argument('-l', '--local', dest='local_ip', required=False, help='Local IP address to check')
-        parser.add_argument('-d', '--domain', dest='domain_scan', required=False, help='Domain address to scan')
-        parser.add_argument('-t', '--tenant', dest='tenant', required=False, help='API tenant')
-        parser.add_argument('-s', '--scan', dest='scan_ip', required=False, action='store_true', help='Initiate local malware scan')
-        parser.add_argument('-b', '--bulk', dest='bulk_scan', required=False, action='store_true', help='Bulk scan')
+    parser = argparse.ArgumentParser(description='Scan IP address using VirusTotal, Shodan and Cortex XDR.')
+    parser.add_argument('-r', '--remote', dest='remote_ip', required=False, help='Remote IP address to scan')
+    parser.add_argument('-l', '--local', dest='local_ip', required=False, help='Local IP address to check')
+    parser.add_argument('-d', '--domain', dest='domain_scan', required=False, help='Domain address to scan')
+    parser.add_argument('-t', '--tenant', dest='tenant', required=False, help='API tenant')
+    parser.add_argument('-s', '--scan', dest='scan_ip', required=False, action='store_true', help='Initiate local malware scan')
+    parser.add_argument('-b', '--bulk', dest='bulk_scan', required=False, action='store_true', help='Bulk scan')
+
+    args = parser.parse_args()
+
+    banner()
     
-        args = parser.parse_args()
+    # Carregando as variáveis de ambiente do arquivo .env
+    load_dotenv(override=True)
+    #.env file content example:
+    #   virustotal_api = "API_KEY"
+    #   shodan_api = "API_KEY"
+    #   abuseipdb_api = "API_KEY"
+    #   cortex_api_tenant = "API_KEY"
+    #   cortex_id_tenant = "ID"
+    #   cortex_fqdn_tenant = "fqdn"
 
-        print("================")
-        print("   socIPcheck   ")
-        print("================")
+    virustotal_api = os.getenv("virustotal_api")
+    shodan_api = os.getenv("shodan_api")
+    abuseipdb_api = os.getenv("abuseipdb_api")
 
-        # Carregando as variáveis de ambiente do arquivo .env
-        load_dotenv(override=True)
-        #.env file content example:
-        #   virustotal_api = "API_KEY"
-        #   shodan_api = "API_KEY"
-        #   abuseipdb_api = "API_KEY"
-        #   cortex_api_tenant = "API_KEY"
-        #   cortex_id_tenant = "ID"
-        #   cortex_fqdn_tenant = "fqdn"
+    ip_pattern = r'^(?:(?:25[0-5]|2[0-4]\d|1?\d{1,2})(?:\.(?!$)|$)){4}$'
 
-        virustotal_api = os.getenv("virustotal_api")
-        shodan_api = os.getenv("shodan_api")
-        abuseipdb_api = os.getenv("abuseipdb_api")
-
-        ip_pattern = r'^(?:(?:25[0-5]|2[0-4]\d|1?\d{1,2})(?:\.(?!$)|$)){4}$'
-
-        if args.bulk_scan:
-            bulk = []
-            print("Insert a domain or remote IP (or press Enter to leave): ")
-            while True:
-                data = input()
-                if data:
-                    bulk.append(data)
-                else:
-                    break
-            print(bulk)
-            for data in bulk:
-                if re.match(ip_pattern, data):
-                    vtScan(data, True, virustotal_api)
-                    abuseIPDB(data, abuseipdb_api)
-                    shodanScan(data, shodan_api)
-                else:
-                    vtScan(data,False,virustotal_api)
-                time.sleep(15) #api limit 4 request per minute
-        if args.remote_ip:
-            abuseIPDB(args.remote_ip, abuseipdb_api)
-            vtScan(args.remote_ip, True, virustotal_api)
-            shodanScan(args.remote_ip, shodan_api)
-        if args.local_ip:
-            if args.tenant:
-                cortex_api = os.getenv(f"cortex_api_{args.tenant}")
-                cortex_id = os.getenv(f"cortex_id_{args.tenant}")
-                cortex_fqdn = os.getenv(f"cortex_fqdn_{args.tenant}")
-                cortexCheck(args.local_ip, cortex_api, cortex_id, cortex_fqdn)
+    if args.local_ip:
+        if args.tenant:
+            cortex_api = os.getenv(f"cortex_api_{args.tenant}")
+            cortex_id = os.getenv(f"cortex_id_{args.tenant}")
+            cortex_fqdn = os.getenv(f"cortex_fqdn_{args.tenant}")
+            cortexScan = cortexCheck(args.local_ip, cortex_api, cortex_id, cortex_fqdn)
+            if cortexScan:
                 if args.scan_ip:
                     cortex_responder_id =  os.getenv(f"cortex_responder_id_{args.tenant}")
                     cortex_responder_api =  os.getenv(f"cortex_responder_api_{args.tenant}")
                     cortexMalwareScan(cortex_responder_api, cortex_responder_id, cortex_fqdn, args.local_ip)
             else:
-                print("You need specified a tenant")
-        if args.domain_scan:
-            vtScan(args.domain_scan, False, virustotal_api)
-    except:
-        print("An exception occurred")
+                try:
+                    dnsRevolv = socket.gethostbyaddr(args.local_ip)[0]
+                    print(f"Machine {dnsRevolv} ({args.local_ip}) is without Cortex.")
+                except:
+                    print(f"Machine {args.local_ip} ({args.local_ip}) didn't resolve name and is without Cortex.")
+        else:
+            print("You need specified a tenant")
+    if args.domain_scan and not args.bulk_scan:
+        vtScan(args.domain_scan, False, virustotal_api)
+    if args.remote_ip and not args.bulk_scan:
+        vtScan(args.remote_ip, True, virustotal_api)
+        abuseIPDB(args.remote_ip, abuseipdb_api)
+        if not shodanScan(args.remote_ip, shodan_api):
+            shodanFreeScan(args.remote_ip)
+    if args.bulk_scan:
+        bulk = []
+        print("\n\nInsert a domain or remote IP (or press Enter to leave): ")
+        while True:
+            data = input()
+            if data:
+                bulk.append(data)
+            else:
+                break
+        print(*bulk, sep=", ")
+        for data in bulk:
+            if re.match(ip_pattern, data):
+                vtScan(data, True, virustotal_api)
+                abuseIPDB(data, abuseipdb_api)
+                shodanScan(data, shodan_api)
+            else:
+                vtScan(data,False,virustotal_api)
+            time.sleep(15) #api limit 4 request per minute
 
 if __name__ == "__main__":
     main()
